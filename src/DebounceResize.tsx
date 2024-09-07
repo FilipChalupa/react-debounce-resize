bc5014f1cb4f20231fae8879b49dae645d7adcb5
Filip Chalupa <chalupa.filip@gmail.com>
2024-09-08 00:03:27 +0200

    Fix reduced motion false positive

--- src/DebounceResize.tsx
+++ src/DebounceResize.tsx
@@ -34,15 +34,33 @@ export const DebounceResize: FunctionComponent<DebounceResizeProps> = ({
 	const prefersReducedMotion = usePrefersReducedMotion()
 	const [isResizing, setIsResizing] = useState(false)
 	const ref = useRef<HTMLDivElement>(null)
-	const firstChangeRef = useRef(true)
+	const lastDimensionsRef = useRef<null | { width: number; height: number }>(
+		null,
+	)
 	const handleChange = useMemo(() => {
 		let timer: ReturnType<typeof setTimeout> | null = null
 
 		return () => {
-			if (firstChangeRef.current === true) {
-				firstChangeRef.current = false
+			const element = ref.current
+			if (!element) {
 				return
 			}
+			const lastDimensions = lastDimensionsRef.current
+			const currentDimensions = {
+				width: element.offsetWidth,
+				height: element.offsetHeight,
+			}
+			lastDimensionsRef.current = currentDimensions
+
+			// Don't debounce on first render or if the dimensions haven't changed
+			if (
+				lastDimensions === null ||
+				(currentDimensions.width === lastDimensions.width &&
+					currentDimensions.height === lastDimensions.height)
+			) {
+				return
+			}
+
 			if (
 				(prefersReducedMotion && !debounceIfReducedMotionPreferredToo) ||
 				debounceMilliseconds === 0
